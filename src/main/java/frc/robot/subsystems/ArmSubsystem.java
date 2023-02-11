18b725703f639e6bebee34f0fd44a29b676c5d2c
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2023-02-11 16:02:44 -0500

    Initial Arm subsystem setup
    Initial Arm subsystem setup

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+
+  public final int/*change to motor class later */ shoulderMotor = 0;
+  public final int /*change to motor class later */ wristMotor = 0;
+  public int shoulderTicks;
+  public int wristTicks;
+
+  /** Creates a new ExampleSubsystem. */
+  public ArmSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    this.shoulderTicks = 0;//read ticks from shaft encoder
+    this.wristTicks = 0;
+
+
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
