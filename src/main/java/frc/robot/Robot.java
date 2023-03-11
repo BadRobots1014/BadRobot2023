7fc5302d60baab2dbd4c2857093ebd7810d29934
Fyrefyte <leowilliamcanales@gmail.com>
2023-03-11 11:30:49 -0500

    These should not be in here

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.RuntopositionCommand;
-import frc.robot.subsystems.ArmSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,9 +16,6 @@ import frc.robot.subsystems.ArmSubsystem;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
-  private RuntopositionCommand m_RuntopositionCommand;
-  private ArmSubsystem m_ArmSubsystem;
 
   private RobotContainer m_robotContainer;
 
