4a6e6324fd830910903696b065eebf8fa14f8d11
Bad Robots Driver Station <badrobots1014@gmail.com>
2024-01-29 20:34:47 -0500

    Started of getDistance cmd for limelight
    Ran out of time didn't finish.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,9 +4,11 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LimelightSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   private final ShuffleboardTab m_tab = Shuffleboard.getTab("Limelight");
+  GenericEntry customTagHeight;
   
   public LimelightSubsystem() {
 
@@ -26,7 +29,9 @@ public class LimelightSubsystem extends SubsystemBase {
     m_tab.addNumber("AprilTag ID: ", this::getAprilTagID);
     m_tab.addNumber("Tag tx", this::getTx);
     m_tab.addNumber("Tag ty", this::getTy);
-  
+
+    m_tab.add("Custom April Tag Height", 0);
+    customTagHeight = m_tab.add("Custom April Tag Height", 0).getEntry();
   }
 
   @Override
@@ -54,6 +59,27 @@ public class LimelightSubsystem extends SubsystemBase {
   public double getTa(){
     double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0.0);
     return ta;
+  }
+
+  public double getTagHeight(){ //returns height of the viewed april tag in inches based off of their expected game field height
+    double customHeight = customTagHeight.getDouble(0);
+    if (customHeight != 0) return customHeight;
+    double id = getAprilTagID();
+
+    if (id == 1 || id == 2 || id == 5 || id == 6 || id == 9 || tag == 10){
+      return 53.38; //height of source and amp april tags
+    }
+    if (id == 3 || id == 4 || id == 5 || id == 7){
+      return 57.13;
+    }
+    else {
+      return 52.00; //remaining tags are all stage april tags
+    }
+  }
+
+  public double getDistance(){
+    double d = (getTagHeight() - camHeight) / Math.tan(getTy()+camAngle);
+    return d;
   }
   
 
